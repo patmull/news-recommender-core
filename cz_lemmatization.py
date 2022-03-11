677568ca416161e1dace385861f90b854b26d143
patmull <patmull@seznam.cz>
2022-03-11 12:24:46 +0100

    Working on wikipedia preprocessing

---
+++ cz_lemmatization.py
@@ -0,0 +1,86 @@
+import re, string
+from nltk import RegexpTokenizer
+import majka
+
+from content_based_recommendation import RecommenderMethods
+from data_conenction import Database
+
+
+class CzLemma:
+
+    def __init__(self):
+        self.df = None
+        self.categories_df = None
+
+    # pre-worked
+    def preprocess(self, sentence, stemming=False, lemma=True):
+        # print(sentence)
+        sentence = str(sentence)
+        sentence = sentence.lower()
+        sentence = sentence.replace('{html}', "")
+        cleanr = re.compile('<.*?>')
+        cleantext = re.sub(cleanr, '', sentence)
+        cleantext.translate(str.maketrans('', '', string.punctuation))  # removing punctuation
+        rem_url = re.sub(r'http\S+', '', cleantext)
+        rem_num = re.sub('[0-9]+', '', rem_url)
+        # print("rem_num")
+        # print(rem_num)
+        tokenizer = RegexpTokenizer(r'\w+')
+        tokens = tokenizer.tokenize(rem_num)
+        # print("tokens")
+        # print(tokens)
+
+        edited_words = [self.cz_lemma(w) for w in tokens]
+        edited_words = list(filter(None, edited_words))  # empty strings removal
+        return " ".join(edited_words)
+
+    def preprocess_single_post_find_by_slug(self, slug, json=False, stemming=False):
+        recommenderMethods = RecommenderMethods()
+        post_dataframe = recommenderMethods.find_post_by_slug(slug)
+        post_dataframe["title"] = post_dataframe["title"].map(lambda s: self.preprocess(s, stemming))
+        post_dataframe["excerpt"] = post_dataframe["excerpt"].map(lambda s: self.preprocess(s, stemming))
+        if json is False:
+            return post_dataframe
+        else:
+            return recommenderMethods.convert_df_to_json(post_dataframe)
+
+    def preprocess_feature(self, feature_text, stemming=False):
+        post_excerpt_preprocessed = self.preprocess(feature_text, stemming)
+        return post_excerpt_preprocessed
+
+    def cz_lemma(self, string, json=False):
+        morph = majka.Majka('morphological_database/majka.w-lt')
+
+        morph.flags |= majka.ADD_DIACRITICS  # find word forms with diacritics
+        morph.flags |= majka.DISALLOW_LOWERCASE  # do not enable to find lowercase variants
+        morph.flags |= majka.IGNORE_CASE  # ignore the word case whatsoever
+        morph.flags = 0  # unset all flags
+
+        morph.tags = False  # return just the lemma, do not process the tags
+        morph.tags = True  # turn tag processing back on (default)
+
+        morph.compact_tag = True  # return tag in compact form (as returned by Majka)
+        morph.compact_tag = False  # do not return compact tag (default)
+
+        morph.first_only = True  # return only the first entry
+        morph.first_only = False  # return all entries (default)
+
+        morph.tags = False
+        morph.first_only = True
+        morph.negative = "ne"
+
+        ls = morph.find(string)
+
+        if json is not True:
+            if not ls:
+                return string
+            else:
+                # # print(ls[0]['lemma'])
+                return str(ls[0]['lemma'])
+        else:
+            return ls
+
+def main():
+    cz_lemma = CzLemma()
+
+if __name__ == "__main__": main()
