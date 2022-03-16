c16639f0abd4953e1ce10d16173ec9bd03fe20e4
patmull <patmull@seznam.cz>
2022-03-16 14:16:26 +0100

    Editing of collaboration based algorithm for RMSE

--- collaboration_based_recommendation.py
+++ collaboration_based_recommendation.py
@@ -5,6 +5,7 @@ from scipy.sparse.linalg import svds
 import numpy as np
 import pandas as pd
 
+
 class Svd:
 
     def __init__(self):
@@ -12,7 +13,7 @@ class Svd:
         self.df_users = None
         self.df_posts = None
         self.user_ratings_mean = None
-        self.user_item_table = None # = R_df_
+        self.user_item_table = None  # = R_df_
 
     def get_user_item_from_db(self):
         database = Database()
@@ -74,7 +75,6 @@ class Svd:
         return R_demeaned
 
     def run_svd(self, user_id, num_of_recommendations=10):
-
         U, sigma, Vt = svds(self.get_user_item_from_db(), k=5)
         # print("U:")
         # print(U)
@@ -96,15 +96,16 @@ class Svd:
         print(preds_df)
 
         preds_df['user_id'] = self.user_item_table.index.values.tolist()
-        preds_df.set_index('user_id', drop=True, inplace=True) # inplace for making change in callable  way
+        preds_df.set_index('user_id', drop=True, inplace=True)  # inplace for making change in callable  way
 
-        already_rated, predictions = self.recommend_posts(preds_df, user_id, self.df_posts, self.df_ratings, num_of_recommendations)
+        already_rated, predictions = self.recommend_posts(preds_df, user_id, self.df_posts, self.df_ratings,
+                                                          num_of_recommendations)
 
         print("already_rated.head(num_of_recommendations)")
-        print(already_rated.head(num_of_recommendations))
+        print(already_rated.head(num_of_recommendations).to_string())
 
         print("List of predictions based on already rated items:")
-        print(predictions.head(num_of_recommendations))
+        print(predictions.head(num_of_recommendations).to_string())
 
         predictions_json = predictions.to_json(orient="split")
 
@@ -114,7 +115,7 @@ class Svd:
 
     def recommend_posts(self, predictions_df, user_id, posts_df, original_ratings_df, num_recommendations):
         # Get and sort the user's predictions
-        user_row_number = user_id # UserID starts at 1, not # 0
+        user_row_number = user_id  # UserID starts at 1, not # 0
 
         # print("predictions_df:")
         # print(predictions_df.to_string())
@@ -123,51 +124,72 @@ class Svd:
         print("sorted_user_predictions")
         print(sorted_user_predictions)
 
-
         # Get the user's data and merge in the post information.
         user_data = original_ratings_df[original_ratings_df.user_id == (user_id)]
         user_full = (user_data.merge(posts_df, how='left', left_on='post_id', right_on='post_id').
                      sort_values(['rating_value'], ascending=False)
-                    )
+                     )
 
-        # print("user_data")
-        # print(user_data)
-        # print("user_full")
-        # print(user_full)
-
-        # print('User {0} has already rated {1} posts.'.format(user_id, user_full.shape[0]))
-        # print('Recommending the highest {0} predicted ratings posts not already rated.'.format(num_recommendations))
-
-        # print("sorted_user_predictions type:")
-        # print(type(sorted_user_predictions))
-
-        """
-        arr = [np.array([posts_df[k] == v for k, v in x.items()]).all(axis=0) for x in sorted_user_predictions.to_dict('r')]
-        recommendations = posts_df[np.array(arr).any(axis=0)]
-        """
-        """
-
-
-        for predicted_post in sorted_user_predictions
-            recommendations_own_version = post_df[predicted_post[0]]
-        """
-        # Recommend the highest predicted rating posts that the user hasn't seen yet.
+        # Recommend the highest predicted rating posts that the user hasn't rated yet.
         recommendations = (posts_df[~posts_df['post_id'].isin(user_full['post_id'])]
                                .merge(pd.DataFrame(sorted_user_predictions).reset_index(),
-                                     how='left',
-                                     left_on='post_id',
-                                     right_on='post_id')
-                                .rename(columns={user_row_number: 'Predictions'})
+                                      how='left',
+                                      left_on='post_id',
+                                      right_on='post_id')
+                               .rename(columns={user_row_number: 'Predictions'})
                                .sort_values('Predictions', ascending=False)
                                .iloc[:num_recommendations, :-1])
 
-        # print("recommendations:")
-        # print(recommendations)
 
         return user_full, recommendations
 
+
+    def rmse(self, user_id):
+        U, sigma, Vt = svds(self.get_user_item_from_db(), k=5)
+        sigma = np.diag(sigma)
+        all_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + self.user_ratings_mean.reshape(-1, 1)
+
+        print("all_user_predicted_ratings")
+        print(all_user_predicted_ratings)
+
+        predictions_df = pd.DataFrame(all_user_predicted_ratings, columns=self.user_item_table.columns)
+
+        print("preds_df")
+        print(predictions_df)
+
+        predictions_df['user_id'] = self.user_item_table.index.values.tolist()
+        predictions_df.set_index('user_id', drop=True, inplace=True)  # inplace for making change in callable  way
+        # Get and sort the user's predictions
+        user_row_number = user_id  # UserID starts at 1, not # 0
+
+        # print("predictions_df:")
+        # print(predictions_df.to_string())
+        sorted_user_predictions = predictions_df.loc[user_row_number].sort_values(ascending=False).to_frame()
+
+        print("sorted_user_predictions")
+        sorted_user_predictions.reset_index(inplace=True)
+        sorted_user_predictions = sorted_user_predictions.rename(columns={user_id: 'rating_value'})
+        print(sorted_user_predictions[['post_id ']])
+        print(sorted_user_predictions[['rating_value']])
+        # print(sorted_user_predictions[["post_id"]])
+        # print(sorted_user_predictions[["rating_value"]])
+
+
+        already_rated, predictions = self.recommend_posts(predictions_df, user_id, self.df_posts, self.df_ratings,
+                                                          10)
+
+        already_rated = already_rated[['post_id','rating_value']]
+
+        print("already_rated")
+        print(already_rated[["post_id"]])
+        print(already_rated[["rating_value"]])
+
+
 def main():
+
     svd = Svd()
-    print(svd.run_svd(431))
+    #print(svd.run_svd(431))
+    print(svd.rmse(431))
+
 
 if __name__ == "__main__": main()
