4e3938c42b49bd42fe486d2e3113dadfdbf55566
patmull <patmull@seznam.cz>
2022-03-30 21:18:03 +0200

    Added full versions to separate files

---
+++ prefilling_lda_full.py
@@ -0,0 +1,8 @@
+from content_based_algorithms.prefiller import PreFiller
+
+while True:
+    try:
+        prefiller = PreFiller()
+        prefiller.prefilling_job("lda", "pgsql", full_text=True, reverse=False, random=True)
+    except Exception as e:
+        print("Exception occured" + str(e))
