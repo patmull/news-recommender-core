981fea12135615e47c99ea10fb0d16a4b848e0cb
patmull <patmull@seznam.cz>
2022-04-27 20:36:40 +0200

    Added better Word2Vec similarity methods according to Gensim official example, added simple evaluation and comparison of Word2Vec between FastText (Wiki) and iDNES trained model

--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ import numpy as np
 from content_based_algorithms.doc2vec import Doc2VecClass
 from content_based_algorithms.helper import Helper
 from content_based_algorithms.tfidf import TfIdf
+from content_based_algorithms.word2vec import Word2VecClass
 
 
 def tfidf():
@@ -29,8 +30,12 @@ def main():
 
     helper = Helper()
     # helper.clear_blank_lines_from_txt("datasets/idnes_preprocessed.txt")
-    doc2vec = Doc2VecClass()
-    print(doc2vec.get_similar_doc2vec("chaos-v-mapach-kavkazu-proc-armenie-a-azerbajdzan-nebojuji-jen-o-karabach", train=True, limited=False))
+    # doc2vec = Doc2VecClass()
+    # print(doc2vec.get_similar_doc2vec("chaos-v-mapach-kavkazu-proc-armenie-a-azerbajdzan-nebojuji-jen-o-karabach", train=True, limited=False))
+    # tfIdf = TfIdf()
+    # print(tfidf.recommend_posts_by_all_features_preprocessed("zemrel-posledni-krkonossky-nosic-helmut-hofer-ikona-velke-upy"))
+    word2vec = Word2VecClass()
+    word2vec.get_similar_word2vec("zemrel-posledni-krkonossky-nosic-helmut-hofer-ikona-velke-upy")
 
 
 if __name__ == '__main__':
