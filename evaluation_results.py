a876d4baec8840247d7a0c34074f1a025221d349
patmull <patmull@seznam.cz>
2022-06-06 23:37:13 +0200

    Starting to collect dataframes for evaluation results LightGBM

---
+++ evaluation_results.py
@@ -0,0 +1,6 @@
+from content_based_algorithms.data_queries import RecommenderMethods
+
+
+def get_results_dataframe():
+    recommenderMethods = RecommenderMethods()
+    return recommenderMethods.get_results_dataframe()  # load posts to dataframe
