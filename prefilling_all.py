dda6582a4eac96c25213df3d16a7c5273620cb9e
patmull <patmull@seznam.cz>
2022-06-17 00:59:26 +0200

    Doc2Vec in prefilling edited

--- prefilling_all.py
+++ prefilling_all.py
@@ -4,52 +4,52 @@ from content_based_algorithms.prefiller import PreFiller
 
 prefiller = PreFiller()
 
-while True:
-    try:
-        prefiller.prefilling_job("tfidf", "pgsql", full_text=False, reverse=True, random=True)
-    except Exception as e:
-        print("Exception occured " + str(e))
-        print(traceback.print_exception(type(e), e, e.__traceback__))
+# while True:
+try:
+    prefiller.prefilling_job("tfidf", "pgsql", full_text=False, reverse=True, random=True)
+except Exception as e:
+    print("Exception occured " + str(e))
+    print(traceback.print_exception(type(e), e, e.__traceback__))
 
-    try:
-        prefiller.prefilling_job("doc2vec", "pgsql", full_text=False, reverse=True, random=True)
-    except Exception as e:
-        print("Exception occured " + str(e))
-        print(traceback.print_exception(type(e), e, e.__traceback__))
+try:
+    prefiller.prefilling_job("doc2vec", "pgsql", full_text=False, reverse=True, random=True)
+except Exception as e:
+    print("Exception occured " + str(e))
+    print(traceback.print_exception(type(e), e, e.__traceback__))
 
-    try:
-        prefiller.prefilling_job("lda", "pgsql", full_text=False, reverse=True, random=True)
-    except Exception as e:
-        print("Exception occured " + str(e))
-        print(traceback.print_exception(type(e), e, e.__traceback__))
-    try:
-        prefiller.prefilling_job("tfidf", "pgsql", full_text=True, reverse=True, random=True)
-    except Exception as e:
-        print("Exception occured " + str(e))
-        print(traceback.print_exception(type(e), e, e.__traceback__))
+try:
+    prefiller.prefilling_job("lda", "pgsql", full_text=False, reverse=True, random=True)
+except Exception as e:
+    print("Exception occured " + str(e))
+    print(traceback.print_exception(type(e), e, e.__traceback__))
+try:
+    prefiller.prefilling_job("tfidf", "pgsql", full_text=True, reverse=True, random=True)
+except Exception as e:
+    print("Exception occured " + str(e))
+    print(traceback.print_exception(type(e), e, e.__traceback__))
 
-    try:
-        prefiller.prefilling_job("doc2vec", "pgsql", full_text=True, reverse=True, random=True)
-    except Exception as e:
-        print("Exception occured " + str(e))
-        print(traceback.print_exception(type(e), e, e.__traceback__))
+try:
+    prefiller.prefilling_job("doc2vec", "pgsql", full_text=True, reverse=True, random=True)
+except Exception as e:
+    print("Exception occured " + str(e))
+    print(traceback.print_exception(type(e), e, e.__traceback__))
 
-    try:
-        prefiller.prefilling_job("lda", "pgsql", full_text=True, reverse=True, random=True)
-    except Exception as e:
-        print("Exception occured " + str(e))
-        print(traceback.print_exception(type(e), e, e.__traceback__))
+try:
+    prefiller.prefilling_job("lda", "pgsql", full_text=True, reverse=True, random=True)
+except Exception as e:
+    print("Exception occured " + str(e))
+    print(traceback.print_exception(type(e), e, e.__traceback__))
 
-    try:
-        prefiller.prefilling_job("word2vec", "pgsql", full_text=False, reverse=True, random=True)
-    except Exception as e:
-        print("Exception occured " + str(e))
-        print(traceback.print_exception(type(e), e, e.__traceback__))
+try:
+    prefiller.prefilling_job("word2vec", "pgsql", full_text=False, reverse=True, random=True)
+except Exception as e:
+    print("Exception occured " + str(e))
+    print(traceback.print_exception(type(e), e, e.__traceback__))
 
-    try:
-        prefiller.prefilling_job("word2vec", "pgsql", full_text=True, reverse=True, random=True)
-    except Exception as e:
-        print("Exception occured " + str(e))
-        
-        print(traceback.print_exception(type(e), e, e.__traceback__))
+try:
+    prefiller.prefilling_job("word2vec", "pgsql", full_text=True, reverse=True, random=True)
+except Exception as e:
+    print("Exception occured " + str(e))
+
+    print(traceback.print_exception(type(e), e, e.__traceback__))
 
