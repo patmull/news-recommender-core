8921207fee206320d3a99ef0b0800f52d05fee25
patmull <patmull@seznam.cz>
2022-06-17 14:00:07 +0200

    Added full_text in selected columns of df in join_post_category() method

--- prefilling_doc2vec.py
+++ prefilling_doc2vec.py
@@ -4,9 +4,10 @@ import psycopg2
 
 from content_based_algorithms.prefiller import PreFiller
 
+prefiller = PreFiller()
+
 while True:
     try:
-        prefiller = PreFiller()
         prefiller.prefilling_job("doc2vec", "pgsql", full_text=False, reverse=False, random=False)
     except Exception as e:
         print("Exception occured " + str(e))
