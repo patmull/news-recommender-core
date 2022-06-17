01984e2b55134f30cd11c989ae3cb81718453e7c
patmull <patmull@seznam.cz>
2022-06-17 10:52:56 +0200

    Added DESC to SQL

--- prefilling_doc2vec.py
+++ prefilling_doc2vec.py
@@ -7,7 +7,7 @@ from content_based_algorithms.prefiller import PreFiller
 while True:
     try:
         prefiller = PreFiller()
-        prefiller.prefilling_job("doc2vec_vectors", "pgsql", full_text=False, reverse=False, random=True)
+        prefiller.prefilling_job("doc2vec_vectors", "pgsql", full_text=False, reverse=False, random=False)
     except Exception as e:
         print("Exception occured " + str(e))
         print(e.with_traceback())
