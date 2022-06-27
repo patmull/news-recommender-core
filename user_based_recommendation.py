e50fba87611217d293a164f26aac9b99223c80e3
patmull <patmull@seznam.cz>
2022-06-27 16:02:44 +0200

    Clearing git cached files for removing gitignored files

--- user_based_recommendation.py
+++ user_based_recommendation.py
@@ -59,6 +59,7 @@ class UserBasedRecommendation:
         return df_user_categories
 
     # loads posts for user based on his id and favourite categories
+    @profile
     def load_recommended_posts_for_user(self, user_id,num_of_recommendations=5):
         self.database.connect()
         df_posts_users_of_categories = self.load_ratings()[self.load_ratings().category_slug.isin(self.load_user_categories(user_id)['category_slug'].tolist())]
