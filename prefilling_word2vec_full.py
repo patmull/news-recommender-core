314cd7e4b053cc378aa4cb7fe60550b81526dfd2
patmull <patmull@seznam.cz>
2022-07-04 19:04:16 +0200

    Started with iDNES.cz Word2Vec evaluation

---
+++ prefilling_word2vec_full.py
@@ -0,0 +1,10 @@
+from content_based_algorithms.prefiller import PreFiller
+
+prefiller = PreFiller()
+
+while True:
+    try:
+        prefiller.prefilling_job("word2vec", "pgsql", full_text=True, reverse=False, random=False)
+    except Exception as e:
+        print("Exception occured " + str(e))
+        print(e)
