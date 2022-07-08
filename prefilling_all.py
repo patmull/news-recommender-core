064e4a459b2bcf6050aa6980f3a3133fda439e1e
patmull <patmull@seznam.cz>
2022-07-08 16:02:37 +0200

    Prefilling edits

--- prefilling_all.py
+++ prefilling_all.py
@@ -2,20 +2,38 @@ import traceback
 
 from content_based_algorithms.prefiller import PreFiller
 from data_connection import Database
+from prefilling_additional import PreFillerAdditional
 
 prefiller = PreFiller()
+prefiller_additional = PreFillerAdditional()
 
 
-# while True:
+def prefill_all_features_preprocessed():
+    prefiller_additional.fill_all_features_preprocessed(skip_already_filled=True, reversed=True, random_order=False)
+
+
+def prefill_keywords():
+    prefiller_additional.fill_keywords(skip_already_filled=True, reversed=True, random_order=False)
+
+
+def prefill_body_preprocessed():
+    prefiller_additional.fill_body_preprocessed(skip_already_filled=True, reversed=True, random_order=False)
+
+
 def run_prefilling():
     print("Running Tfidf short text")
 
-    # TODO: Check needed columns
-    # TODO: 'all_features_preprocessed' (probably every method relies on this)
-    # TODO: 'keywords' (LDA but probably also other columns relies on this)
-    # TODO: 'body_preprocessed' (LDA relies on this)
-
     database = Database()
+    columns_needing_prefill = check_needed_columns(database)
+
+    if len(columns_needing_prefill) > 0:
+        if 'all_features_preprocessed' in columns_needing_prefill:
+            prefill_all_features_preprocessed()
+        if 'keywords' in columns_needing_prefill:
+            prefill_keywords()
+        if 'body_preprocessed' in columns_needing_prefill:
+            prefill_body_preprocessed()
+
     reverse = True
     random = False
 
@@ -61,3 +79,25 @@ def prepare_and_run(database, method, full_text, reverse, random):
     else:
         print("No not prefilled posts found")
         print("Skipping " + method + " full text")
+
+
+def check_needed_columns(database):
+    # TODO: Check needed columns
+    # TODO: 'all_features_preprocessed' (probably every method relies on this)
+    # TODO: 'keywords' (LDA but probably also other columns relies on this)
+    # TODO: 'body_preprocessed' (LDA relies on this)
+    needed_checks = []
+    number_of_nans_in_all_features_preprocessed = database.df['all_features_preprocessed'].isna().sum()
+    number_of_nans_in_keywords = database.df['keywords'].isna().sum()
+    number_of_nans_in_body_preprocessed = database.df['body_preprocessed'].isna().sum()
+
+    if number_of_nans_in_all_features_preprocessed:
+        needed_checks.extend("all_features_preprocessed")
+    if number_of_nans_in_keywords:
+        needed_checks.extend("keywords")
+    if number_of_nans_in_body_preprocessed:
+        needed_checks.extend("body_preprocessed")
+
+    print("Values missing in:")
+    print(str(needed_checks))
+    return needed_checks
