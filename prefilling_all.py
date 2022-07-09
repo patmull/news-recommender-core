9fabb1466287c7be3ae44aba8124570a1c136fe3
patmull <patmull@seznam.cz>
2022-07-09 18:06:51 +0200

    Added SQL commands for investigation of missing values in prefilled columns and deprecation warning

--- prefilling_all.py
+++ prefilling_all.py
@@ -68,7 +68,7 @@ def run_prefilling():
 
 def prepare_and_run(database, method, full_text, reverse, random):
     not_prefilled_posts = database.get_not_prefilled_posts(method=method, full_text=full_text)
-    print("Found " + str(len(not_prefilled_posts)) + " not prefilled posts")
+    print("Found " + str(len(not_prefilled_posts)) + " not prefilled posts in " + method)
     if len(not_prefilled_posts) > 0:
         try:
             prefiller.prefilling_job(method=method, full_text=full_text)
