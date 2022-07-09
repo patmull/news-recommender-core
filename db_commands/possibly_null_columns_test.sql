87855f843030782d376b48fa2e99f410c4ae4089
patmull <patmull@seznam.cz>
2022-07-09 17:45:01 +0200

    Added recommended SQL commands

--- db_commands/possibly_null_columns_test.sql
+++ db_commands/possibly_null_columns_test.sql
@@ -5,3 +5,12 @@ SELECT keywords FROM posts WHERE keywords IS NULL;
 SELECT all_feature_preprocessed FROM posts WHERE body_preprocessed IS NULL;
 
 SELECT * FROM posts WHERE NOT (posts IS NOT NULL);
+
+SELECT COUNT (*) AS "recommended_tfidf" FROM posts WHERE posts.recommended_tfidf IS NULL;
+SELECT COUNT (*) AS "recommended_tfidf_full_text" FROM posts WHERE posts.recommended_tfidf_full_text IS NULL;
+SELECT COUNT (*) AS "recommended_word2vec" FROM posts WHERE posts.recommended_word2vec IS NULL;
+SELECT COUNT (*) AS "recommended_word2vec_full_text" FROM posts WHERE posts.recommended_word2vec_full_text IS NULL;
+SELECT COUNT (*) AS "recommended_doc2vec" FROM posts WHERE posts.recommended_doc2vec IS NULL;
+SELECT COUNT (*) AS "recommended_doc2vec_full_text" FROM posts WHERE posts.recommended_doc2vec_full_text IS NULL;
+SELECT COUNT (*) AS "recommended_lda" FROM posts WHERE posts.recommended_lda IS NULL;
+SELECT COUNT (*) AS "recommended_lda_full_text" FROM posts WHERE posts.recommended_lda_full_text IS NULL;
