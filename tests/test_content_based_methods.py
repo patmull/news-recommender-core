01673b2f581d01bcb46e624ceb01b875fd94a5ff
patmull <patmull@seznam.cz>
2022-07-10 11:48:11 +0200

    Doc2Vec full text category posts columns name problems fixed

--- tests/test_content_based_methods.py
+++ tests/test_content_based_methods.py
@@ -1,3 +1,4 @@
+from content_based_algorithms.doc2vec import Doc2VecClass
 from content_based_algorithms.word2vec import Word2VecClass
 from data_connection import Database
 
@@ -26,3 +27,31 @@ def test_word2vec_method():
     assert type(similar_posts[0]['slug']) is str
     assert type(similar_posts[0]['coefficient']) is float
     assert len(similar_posts) > 0
+
+
+def test_doc2vec_full_text_method():
+    doc2vec = Doc2VecClass()
+    # random article
+    database = Database()
+    posts = database.get_posts_dataframe()
+    random_post = posts.sample()
+    random_post_slug = random_post['slug'].iloc[0]
+    print("random_post slug:")
+    print(random_post_slug)
+    similar_posts = doc2vec.get_similar_doc2vec_with_full_text(random_post_slug)
+    print("similar_posts:")
+    print(similar_posts)
+    print("similar_posts type:")
+    print(type(similar_posts))
+
+    assert len(random_post.index) == 1
+    assert type(similar_posts) is list
+    assert len(similar_posts) > 0
+    print(type(similar_posts[0]['slug']))
+    assert type(similar_posts[0]['slug']) is str
+    assert type(similar_posts[0]['coefficient']) is float
+    assert len(similar_posts) > 0
+
+# TODO:
+#  recommended_doc2vec_full_text
+#  recommended_lda_full_text
