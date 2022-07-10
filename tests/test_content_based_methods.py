e762881156a3ec7a731e4812cb8ed8bb837d3263
patmull <patmull@seznam.cz>
2022-07-10 13:13:23 +0200

    Lda full text fixed

--- tests/test_content_based_methods.py
+++ tests/test_content_based_methods.py
@@ -1,4 +1,5 @@
 from content_based_algorithms.doc2vec import Doc2VecClass
+from content_based_algorithms.lda import Lda
 from content_based_algorithms.word2vec import Word2VecClass
 from data_connection import Database
 
@@ -52,6 +53,26 @@ def test_doc2vec_full_text_method():
     assert type(similar_posts[0]['coefficient']) is float
     assert len(similar_posts) > 0
 
-# TODO:
-#  recommended_doc2vec_full_text
-#  recommended_lda_full_text
+
+def test_lda_full_text_method():
+    lda = Lda()
+    # random article
+    database = Database()
+    posts = database.get_posts_dataframe()
+    random_post = posts.sample()
+    random_post_slug = random_post['slug'].iloc[0]
+    print("random_post slug:")
+    print(random_post_slug)
+    similar_posts = lda.get_similar_lda_full_text(random_post_slug)
+    print("similar_posts:")
+    print(similar_posts)
+    print("similar_posts type:")
+    print(type(similar_posts))
+
+    assert len(random_post.index) == 1
+    assert type(similar_posts) is list
+    assert len(similar_posts) > 0
+    print(type(similar_posts[0]['slug']))
+    assert type(similar_posts[0]['slug']) is str
+    assert type(similar_posts[0]['coefficient']) is float
+    assert len(similar_posts) > 0
