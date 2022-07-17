f30058e00623160784d2352951ff16ef2aa48905
patmull <patmull@seznam.cz>
2022-07-17 12:19:00 +0200

    Aded method for and preprocessed questions-word-cs files

--- try.py
+++ try.py
@@ -1,8 +1,10 @@
 import warnings
 
+from gensim.models import Word2Vec
+
 from content_based_algorithms.data_queries import RecommenderMethods
 from content_based_algorithms.lda import Lda
-from content_based_algorithms.word2vec import Word2VecClass
+from content_based_algorithms.word2vec import Word2VecClass, preprocess_question_words_file
 from data_connection import Database
 from prefilling_all import prepare_and_run, run_prefilling
 
@@ -41,3 +43,24 @@ with warnings.catch_warnings():
 
 word2vecClass = Word2VecClass()
 word2vecClass.find_optimal_model_idnes()
+
+# preprocess_question_words_file()
+
+"""
+path_to_cropped_wordsim_file = 'research/word2vec/similarities/WordSim353-cs-cropped.tsv'
+w2v_model = Word2Vec.load("models/w2v_idnes.model")
+word_pairs_eval = w2v_model.wv.evaluate_word_pairs(path_to_cropped_wordsim_file, case_insensitive=True)
+print("Word pairs test:")
+print('Word_pairs_test_Pearson_coeff: ' + str(word_pairs_eval[0][0]))
+print('Word_pairs_test_Pearson_p-val: ' + str(word_pairs_eval[0][1]))
+print('Word_pairs_test_Spearman_coeff: ' + str(word_pairs_eval[1][0]))
+print('Word_pairs_test_Spearman_p-val: ' + str(word_pairs_eval[1][1]))
+print('Word_pairs_test_Out-of-vocab_ratio: ' + str(word_pairs_eval[2]))
+print(word_pairs_eval)
+print("Similarity test:")
+print(w2v_model.wv.most_similar('tygr'))
+overall_score, _ = w2v_model.wv.evaluate_word_analogies('research/word2vec/analogies/questions-words-cs.txt', case_insensitive=True)
+print("Analogies evaluation of iDnes.cz model:")
+print(overall_score)
+
+"""
