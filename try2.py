f88689a2cf383074f594c1201efaa3982b95df6e
patmull <patmull@seznam.cz>
2022-08-05 11:35:01 +0200

    -

--- try2.py
+++ try2.py
@@ -37,10 +37,8 @@ with warnings.catch_warnings():
     run_prefilling() 
 """
 
-
 word2vec = Word2VecClass()
 word2vec.final_training_idnes_model()
-
 
 """
 word2vec = Word2VecClass()
@@ -76,23 +74,3 @@ w2v_model.eval_wiki()
 bigram_phrases = BigramPhrases()
 bigram_phrases.train_phrases_from_mongo_idnes()
 """
-"""
-path_to_cropped_wordsim_file = 'research/word2vec/similarities/WordSim353-cs-cropped.tsv'
-path_to_cropped_wordsim_file = 'models/w2v_model_limited'
-w2v_model = Word2Vec.load("models/w2v_idnes.model")
-word_pairs_eval = w2v_model.wv.evaluate_word_pairs(path_to_cropped_wordsim_file, case_insensitive=True)
-print("Word pairs test:")
-print('Word_pairs_test_Pearson_coeff: ' + str(word_pairs_eval[0][0]))
-print('Word_pairs_test_Pearson_p-val: ' + str(word_pairs_eval[0][1]))
-print('Word_pairs_test_Spearman_coeff: ' + str(word_pairs_eval[1][0]))
-print('Word_pairs_test_Spearman_p-val: ' + str(word_pairs_eval[1][1]))
-print('Word_pairs_test_Out-of-vocab_ratio: ' + str(word_pairs_eval[2]))
-print(word_pairs_eval)
-print("Similarity test:")
-print(w2v_model.wv.most_similar('tygr'))
-overall_score, _ = w2v_model.wv.evaluate_word_analogies('research/word2vec/analogies/questions-words-cs.txt', case_insensitive=True)
-print("Analogies evaluation of iDnes.cz model:")
-print(overall_score)
-"""
-
-# try_ap()
