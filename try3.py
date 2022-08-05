6a8d6cb692579587c3e4ef0b2a6e53d842111944
patmull <patmull@seznam.cz>
2022-08-05 11:35:01 +0200

    -

--- try3.py
+++ try3.py
@@ -4,7 +4,6 @@ from preprocessing.bigrams_phrases import BigramPhrases
 from data_connection import Database
 from prefilling_all import prepare_and_run, run_prefilling
 
-
 def try_word2vec_recommendation_prefiller(database, method, full_text, reverse, random):
     prepare_and_run(database, method, full_text, reverse, random)
 
@@ -75,24 +74,6 @@ w2v_model.eval_wiki()
 
 bigram_phrases = BigramPhrases()
 bigram_phrases.train_phrases_from_mongo_idnes()
-"""
-"""
-path_to_cropped_wordsim_file = 'research/word2vec/similarities/WordSim353-cs-cropped.tsv'
-path_to_cropped_wordsim_file = 'models/w2v_model_limited'
-w2v_model = Word2Vec.load("models/w2v_idnes.model")
-word_pairs_eval = w2v_model.wv.evaluate_word_pairs(path_to_cropped_wordsim_file, case_insensitive=True)
-print("Word pairs test:")
-print('Word_pairs_test_Pearson_coeff: ' + str(word_pairs_eval[0][0]))
-print('Word_pairs_test_Pearson_p-val: ' + str(word_pairs_eval[0][1]))
-print('Word_pairs_test_Spearman_coeff: ' + str(word_pairs_eval[1][0]))
-print('Word_pairs_test_Spearman_p-val: ' + str(word_pairs_eval[1][1]))
-print('Word_pairs_test_Out-of-vocab_ratio: ' + str(word_pairs_eval[2]))
-print(word_pairs_eval)
-print("Similarity test:")
-print(w2v_model.wv.most_similar('tygr'))
-overall_score, _ = w2v_model.wv.evaluate_word_analogies('research/word2vec/analogies/questions-words-cs.txt', case_insensitive=True)
-print("Analogies evaluation of iDnes.cz model:")
-print(overall_score)
 """
 
 # try_ap()
