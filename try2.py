ccd698c09bd933543d86e51c09cd4164179d01df
patmull <patmull@seznam.cz>
2022-08-08 09:57:01 +0200

    Finding optimal model for Word2vec Wiki

--- try2.py
+++ try2.py
@@ -36,9 +36,13 @@ with warnings.catch_warnings():
     recommender_methods.database.insert_posts_dataframe_to_cache()
     run_prefilling() 
 """
-
 word2vec = Word2VecClass()
 word2vec.final_training_idnes_model()
+
+"""
+doc2vec = Doc2VecClass()
+doc2vec.train_final_doc2vec_model_idnes()
+"""
 
 """
 word2vec = Word2VecClass()
