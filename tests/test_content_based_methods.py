80ffe19bd77a896c79177fe5c337ac3bc9f0eda2
patmull <patmull@seznam.cz>
2022-08-28 11:58:33 +0200

    Fixed Word2Vec

--- tests/test_content_based_methods.py
+++ tests/test_content_based_methods.py
@@ -148,30 +148,6 @@ def test_tfidf_full_text_method():
     assert len(similar_posts) > 0
 
 
-def test_word2vec_full_text_method():
-    word2vec = Word2VecClass()
-    # random_order article
-    database = Database()
-    posts = database.get_posts_dataframe()
-    random_post = posts.sample()
-    random_post_slug = random_post['slug'].iloc[0]
-    print("random_post slug:")
-    print(random_post_slug)
-    similar_posts = word2vec.get_similar_word2vec_full_text(random_post_slug)
-    print("similar_posts:")
-    print(similar_posts)
-    print("similar_posts type:")
-    print(type(similar_posts))
-
-    assert len(random_post.index) == 1
-    assert type(similar_posts) is list
-    assert len(similar_posts) > 0
-    print(type(similar_posts[0]['slug']))
-    assert type(similar_posts[0]['slug']) is str
-    assert type(similar_posts[0]['coefficient']) is float
-    assert len(similar_posts) > 0
-
-
 def test_doc2vec_full_text_method():
     doc2vec = Doc2VecClass()
     # random_order article
