064c6ffe9884e8cf28e97ab112ea2350611b0bf3
patmull <patmull@seznam.cz>
2022-09-01 16:52:20 +0200

    Setting-up syncing between API-CORE and FRESH-API

--- src/data_manipulation.py
+++ src/data_manipulation.py
@@ -152,7 +152,8 @@ class Database:
     def get_posts_dataframe_from_cache(self):
         print("Reading cache file...")
         try:
-            df = Path('db_cache/cached_posts_dataframe.pkl')
+            path_to_df = Path('db_cache/cached_posts_dataframe.pkl')
+            df = pd.read_pickle(path_to_df)
             # read from current directory
         except Exception as e:
             print("Exception occured when reading cached file:")
