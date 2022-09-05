551f2ceaff0ace36be510a612d3bf46684b7da82
patmull <patmull@seznam.cz>
2022-09-05 17:05:10 +0200

    Added methods for user thumbs item assesed relevance

---
+++ src/core/recommender_algorithms/hybrid/relevance_results.py
@@ -0,0 +1,11 @@
+from src.core.data_handling.data_queries import RecommenderMethods
+
+
+def get_admin_evaluation_results_dataframe():
+    recommender_methods = RecommenderMethods()
+    return recommender_methods.get_ranking_evaluation_results_dataframe()  # load_texts posts to dataframe
+
+
+def get_user_evaluation_results_dataframe():
+    recommender_methods = RecommenderMethods()
+    return recommender_methods.get_item_evaluation_results_dataframe()  # load_texts posts to dataframe
