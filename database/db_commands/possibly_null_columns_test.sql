53933abd7efe3361eb76c02b559f85b15b7707c8
patmull <patmull@seznam.cz>
2022-09-06 19:21:19 +0200

    Added loading of model

--- database/db_commands/possibly_null_columns_test.sql
+++ database/db_commands/possibly_null_columns_test.sql
@@ -2,7 +2,7 @@ SELECT keywords FROM posts WHERE slug = 'za-tyden-jsme-s-elektromobilem-ujeli-3-
 
 SELECT body_preprocessed FROM posts WHERE body_preprocessed IS NULL;
 SELECT keywords FROM posts WHERE keywords IS NULL;
-SELECT all_feature_preprocessed FROM posts WHERE body_preprocessed IS NULL;
+SELECT all_features_preprocessed FROM posts WHERE body_preprocessed IS NULL;
 
 SELECT * FROM posts WHERE NOT (posts IS NOT NULL);
 
