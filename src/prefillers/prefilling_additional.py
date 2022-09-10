7cecbb264d7bc1e897b5377cf9b69fa9779eff2a
patmull <patmull@seznam.cz>
2022-09-10 21:11:54 +0200

    Code inspection: duplicated code solving

--- src/prefillers/prefilling_additional.py
+++ src/prefillers/prefilling_additional.py
@@ -23,14 +23,7 @@ class PreFillerAdditional:
 
         number_of_inserted_rows = 0
 
-        if reversed is True:
-            print("Reversing list of posts...")
-            posts_categories.reverse()
-
-        if random_order is True:
-            print("Starting random_order iteration...")
-            time.sleep(5)
-            random.shuffle(posts_categories)
+        self.shuffle_and_reverse(posts=posts, reversed_order=reversed, random_order=random_order)
 
         cz_lemma = CzPreprocess()
 
@@ -49,7 +42,7 @@ class PreFillerAdditional:
             article_body_preprocessed = post[8]
 
             print("Prefilling body preprocessd in article: " + slug)
-    
+
             if skip_already_filled is True:
                 if article_body_preprocessed is None:
                     if db == "redis":
@@ -69,21 +62,8 @@ class PreFillerAdditional:
                 else:
                     print("Skipping.")
             else:
-                if db == "pgsql":
-                    preprocessed_text = cz_lemma.preprocess(article_full_text)
-                    print("article_full_text")
-                    print(article_full_text)
-                    print("preprocessed_text:")
-                    print(preprocessed_text)
-
-                    database.insert_preprocessed_body(preprocessed_body=preprocessed_text, article_id=post_id)
-                else:
-                    raise Exception
-                number_of_inserted_rows += 1
-                if number_of_inserted_rows > 20:
-                    print("Refreshing list of posts for finding only not prefilled posts.")
-                    self.fill_all_features_preprocessed(db=db, skip_already_filled=True, reversed=reversed,
-                                                        random_order=random_order)
+                self.start_preprocessed_features_prefilling(db, cz_lemma, article_full_text, database, post_id,
+                                                            number_of_inserted_rows, random_order)
 
     def fill_body_preprocessed(self, skip_already_filled, reversed, random_order, db="pgsql"):
         database = DatabaseMethods()
@@ -94,26 +74,16 @@ class PreFillerAdditional:
 
         number_of_inserted_rows = 0
 
-        if reversed is True:
-            print("Reversing list of posts...")
-            posts.reverse()
-
-        if random_order is True:
-            print("Starting random_order iteration...")
-            time.sleep(5)
-            random.shuffle(posts)
+        self.shuffle_and_reverse(posts=posts, reversed_order=reversed, random_order=random_order)
 
         cz_lemma = CzPreprocess()
 
         for post in posts:
             if len(posts) < 1:
                 break
-            post_id = post[0]
-            slug = post[3]
-            article_title = post[2]
-            article_excerpt = post[4]
-            article_full_text = post[20]
-            current_body_preprocessed = post[21]
+
+            post_id, slug, article_title, article_excerpt, article_full_text, \
+            current_body_preprocessed = self.get_post_columns(post)
 
             print("Prefilling body preprocessd in article: " + slug)
 
@@ -132,26 +102,13 @@ class PreFillerAdditional:
                 else:
                     print("Skipping.")
             else:
-                if db == "pgsql":
-                    preprocessed_text = cz_lemma.preprocess(article_full_text)
-                    print("article_full_text")
-                    print(article_full_text)
-                    print("preprocessed_text:")
-                    print(preprocessed_text)
-
-                    database.insert_preprocessed_body(preprocessed_body=preprocessed_text, article_id=post_id)
-                else:
-                    raise NotImplementedError
-                number_of_inserted_rows += 1
-                if number_of_inserted_rows > 20:
-                    print("Refreshing list of posts for finding only not prefilled posts.")
-                    self.fill_all_features_preprocessed(db=db, skip_already_filled=True, reversed=reversed,
-                                                        random_order=random_order)
+                self.start_preprocessed_features_prefilling(db, cz_lemma, article_full_text, database, post_id,
+                                                       number_of_inserted_rows, random_order)
 
     def fill_keywords(self, skip_already_filled, reversed, random_order):
         pass
 
-    def fill_all_features_preprocessed(self, skip_already_filled, reversed, random_order, db="pgsql"):
+    def fill_all_features_preprocessed(self, skip_already_filled, reversed_order, random_order, db="pgsql"):
         database = DatabaseMethods()
         if skip_already_filled is False:
             posts = database.get_all_posts()
@@ -160,27 +117,16 @@ class PreFillerAdditional:
 
         number_of_inserted_rows = 0
 
-        if reversed is True:
-            print("Reversing list of posts...")
-            posts.reverse()
-
-        if random_order is True:
-            print("Starting random_order iteration...")
-            time.sleep(5)
-            random.shuffle(posts)
+        self.shuffle_and_reverse(posts=posts, reversed_order=reversed_order, random_order=random_order)
 
         cz_lemma = CzPreprocess()
 
         for post in posts:
             if len(posts) < 1:
                 break
-            post_id = post[0]
-            slug = post[3]
-            article_title = post[2]
-            article_excerpt = post[4]
-            article_full_text = post[20]
-            current_body_preprocessed = post[21]
             # TODO: Category should be there too
+            post_id, slug, article_title, article_excerpt, article_full_text, \
+            current_body_preprocessed = self.get_post_columns(post)
 
             print("Prefilling body preprocessd in article: " + slug)
 
@@ -199,18 +145,46 @@ class PreFillerAdditional:
                 else:
                     print("Skipping.")
             else:
-                if db == "pgsql":
-                    preprocessed_text = cz_lemma.preprocess(article_full_text)
-                    print("article_full_text")
-                    print(article_full_text)
-                    print("preprocessed_text:")
-                    print(preprocessed_text)
+                self.start_preprocessed_features_prefilling(self, db, cz_lemma, article_full_text, database, post_id,
+                                                       number_of_inserted_rows, random_order)
 
-                    database.insert_preprocessed_body(preprocessed_body=preprocessed_text, article_id=post_id)
-                else:
-                    raise NotImplementedError
-                number_of_inserted_rows += 1
-                if number_of_inserted_rows > 20:
-                    print("Refreshing list of posts for finding only not prefilled posts.")
-                    self.fill_all_features_preprocessed(db=db, skip_already_filled=True, reversed=reversed,
-                                                        random_order=random_order)
+    def shuffle_and_reverse(self, posts, reversed_order, random_order):
+        number_of_inserted_rows = 0
+
+        if reversed_order is True:
+            print("Reversing list of posts...")
+            posts.reverse()
+
+        if random_order is True:
+            print("Starting random_order iteration...")
+            time.sleep(5)
+            random.shuffle(posts)
+
+    def start_preprocessed_features_prefilling(self, db, cz_lemma, article_full_text, database, post_id,
+                                               number_of_inserted_rows, random_order):
+        if db == "pgsql":
+            preprocessed_text = cz_lemma.preprocess(article_full_text)
+            print("article_full_text")
+            print(article_full_text)
+            print("preprocessed_text:")
+            print(preprocessed_text)
+
+            database.insert_preprocessed_body(preprocessed_body=preprocessed_text, article_id=post_id)
+        else:
+            raise Exception
+        number_of_inserted_rows += 1
+        if number_of_inserted_rows > 20:
+            print("Refreshing list of posts for finding only not prefilled posts.")
+            self.fill_all_features_preprocessed(db=db, skip_already_filled=True, reversed_order=reversed,
+                                                random_order=random_order)
+
+    def get_post_columns(self, post):
+
+        post_id = post[0]
+        slug = post[3]
+        article_title = post[2]
+        article_excerpt = post[4]
+        article_full_text = post[20]
+        current_body_preprocessed = post[21]
+
+        return post_id, slug, article_title, article_excerpt, article_full_text, current_body_preprocessed
---
+++ src/recommender_core/data_handling/data_connection.py
@@ -0,0 +1,37 @@
+import os
+import ssl
+import pika
+
+
+def init_rabbimq():
+    ssl_enabled = os.environ.get("ssl", False)
+
+    rabbitmq_user = os.environ.get('CLOUDAMQP_USERNAME', '***REMOVED***')
+    rabbitmq_password = os.environ.get('CLOUDAMQP_PASSWORD', '***REMOVED***')
+    rabbitmq_host = os.environ.get('CLOUDAMQP_HOST', '***REMOVED***')
+    rabbitmq_vhost = os.environ.get('CLOUDAMQP_VHOST', rabbitmq_user)
+
+    if ssl_enabled:
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        context.verify_mode = ssl.CERT_REQUIRED
+        context.load_verify_locations(os.environ.get("ca_bundle", '/etc/pki/tls/certs/ca-bundle.crt'))
+        ssl_options = pika.SSLOptions(context)
+        port = os.environ.get("port", 5671)
+    else:
+        ssl_options = None
+        port = os.environ.get("port", 5672)
+
+    credentials = pika.credentials.PlainCredentials(
+        username=rabbitmq_user, password=rabbitmq_password
+    )
+    connection_params = pika.ConnectionParameters(
+        host=rabbitmq_host,
+        ssl_options=ssl_options,
+        port=port,
+        credentials=credentials,
+        virtual_host=rabbitmq_vhost,
+        heartbeat=600,
+        blocked_connection_timeout=300
+    )
+
+    return pika.BlockingConnection(connection_params)
