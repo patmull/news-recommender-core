702e203ce1e953bf72344358fb983d0043f319f5
patmull <patmull@seznam.cz>
2022-09-12 20:19:16 +0200

    Redis

--- tests/test_integration/test_recommender_methods/test_content_based_methods.py
+++ tests/test_integration/test_recommender_methods/test_content_based_methods.py
@@ -1,6 +1,5 @@
 import pytest
 
-from src.recommender_core.data_handling.data_queries import RecommenderMethods
 from src.recommender_core.recommender_algorithms.content_based_algorithms.doc2vec import Doc2VecClass
 from src.recommender_core.recommender_algorithms.content_based_algorithms.lda import Lda
 from src.recommender_core.data_handling.data_manipulation import DatabaseMethods
@@ -33,7 +32,7 @@ def test_tfidf_method():
     tfidf = TfIdf()
     # random_order article
     database = DatabaseMethods()
-    posts = database.get_posts_dataframe()
+    posts = database.get_posts_dataframe(from_cache=False)
     random_post = posts.sample()
     random_post_slug = random_post['slug'].iloc[0]
     print("random_post slug:")
@@ -103,7 +102,7 @@ def test_doc2vec_method_for_random_post():
     doc2vec = Doc2VecClass()
     # random_order article
     database = DatabaseMethods()
-    posts = database.get_posts_dataframe()
+    posts = database.get_posts_dataframe(from_cache=False)
     random_post = posts.sample()
     random_post_slug = random_post['slug'].iloc[0]
     print("random_post slug:")
@@ -174,7 +173,7 @@ def test_tfidf_full_text_method():
     tfidf = TfIdf()
     # random_order article
     database = DatabaseMethods()
-    posts = database.get_posts_dataframe()
+    posts = database.get_posts_dataframe(from_cache=False)
     random_post = posts.sample()
     random_post_slug = random_post['slug'].iloc[0]
     print("random_post slug:")
@@ -208,28 +207,3 @@ def test_doc2vec_full_text_method_bad_inputs(tested_input):
     with pytest.raises(ValueError):
         doc2vec = Doc2VecClass()
         doc2vec.get_similar_doc2vec_with_full_text(tested_input, posts_from_cache=False)
-
-
-@pytest.mark.integtest
-def test_doc2vec_full_text_method():
-    doc2vec = Doc2VecClass()
-    # random_order article
-    database = DatabaseMethods()
-    posts = database.get_posts_dataframe()
-    random_post = posts.sample()
-    random_post_slug = random_post['slug'].iloc[0]
-    print("random_post slug:")
-    print(random_post_slug)
-    similar_posts = doc2vec.get_similar_doc2vec_with_full_text(random_post_slug, posts_from_cache=False)
-    print("similar_posts:")
-    print(similar_posts)
-    print("similar_posts type:")
-    print(type(similar_posts))
-
-    assert len(random_post.index) == 1
-    assert type(similar_posts) is list
-    assert len(similar_posts) > 0
-    print(type(similar_posts[0]['slug']))
-    assert type(similar_posts[0]['slug']) is str
-    assert type(similar_posts[0]['coefficient']) is float
-    assert len(similar_posts) > 0
