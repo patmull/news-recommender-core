b97d3de563fb6a4e122e78e0f43ff3b8b11dcb17
patmull <patmull@seznam.cz>
2022-09-13 17:55:02 +0200

    Fixed Word2Vec
     Please enter the commit message for your changes. Lines starting

--- try.py
+++ try.py
@@ -14,29 +14,10 @@ assert len(user_categories_thumbs_df.index) > 0  # assert there are rows in data
 database.disconnect()
 """
 
-# TODO: Add test user who will have some thumbs from posts that are already prefilled...
+# TODO: Add test user who will have some thumbs from posts that have already prefilled BERT vectors...
 # (Currently there are no thumb rated posts that are prefilled)
 """
 svm = Classifier()
 svm.predict_relevance_for_user(use_only_sample_of=20, user_id=431, relevance_by='thumbs')
 """
 # TODO: Unit test bad input handling
-"""
-svm = Classifier()
-svm.predict_relevance_for_user(use_only_sample_of=20, user_id=431, relevance_by='stars')
-svm.predict_relevance_for_user(use_only_sample_of=20, user_id=431, relevance_by='thumbs')
-"""
-
-"""
-r = get_redis_connection()
-now = datetime.now()
-test_value = 'test_' + str(now.strftime("%m/%d/%Y %H:%M:%S"))
-r.set('test_pair', test_value)
-assert r.get('test_pair').decode() == test_value
-r.delete("posts_by_pred_ratings_user_371")
-test_value = "vytvorili-prvni-rib-eye-steak-ze-zkumavky-chutna-jako-prave-maso"
-test_user = "posts_by_pred_ratings_user_371"
-res = r.sadd(test_value, test_user)
-print(res)
-print(r.smembers(test_value))
-"""
