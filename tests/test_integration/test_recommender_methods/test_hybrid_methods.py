92afaa731572325c56062ac5398ed9c400c45c01
patmull <patmull@seznam.cz>
2022-09-17 20:20:33 +0200

    Moved new hybrid file to hybrid folder

--- tests/test_integration/test_recommender_methods/test_hybrid_methods.py
+++ tests/test_integration/test_recommender_methods/test_hybrid_methods.py
@@ -1,7 +1,11 @@
+import json
+
 import numpy as np
 import pandas as pd
 import pytest
 
+from recommender_core.recommender_algorithms.hybrid_algorithms.hybrid_methods import \
+    get_most_similar_from_tfidf_matrix
 from src.recommender_core.recommender_algorithms.user_based_algorithms.user_relevance_classifier.classifier import \
     load_bert_model
 from src.recommender_core.recommender_algorithms.content_based_algorithms.doc2vec import Doc2VecClass
@@ -47,3 +51,20 @@ def test_thumbs():
     THUMBS_COLUMNS_NEEDED = ['thumbs_values', 'thumbs_created_at', 'all_features_preprocessed', 'full_text']
     assert all(elem in user_categories_thumbs_df.columns.values for elem in THUMBS_COLUMNS_NEEDED)
     assert len(user_categories_thumbs_df.index) > 0  # assert there are rows in dataframe
+
+def test_hybrid_by_svd_history_tfidf():
+    test_user_id = 431
+    searched_slug_1 = "zemrel-posledni-krkonossky-nosic-helmut-hofer-ikona-velke-upy"
+    searched_slug_2 = "salah-pomohl-hattrickem-ztrapnit-united-soucek-byl-u-vyhry-nad-tottenhamem"
+    searched_slug_3 = "sileny-cesky-plan-dva-roky-trenoval-ted-chce-sam-preveslovat-atlantik"
+
+    test_slugs = [searched_slug_1, searched_slug_2, searched_slug_3]
+    most_similar_hybrid_by_tfidf = get_most_similar_from_tfidf_matrix(user_id=test_user_id, posts_to_compare=test_slugs)
+    type_of_json = type(most_similar_hybrid_by_tfidf)
+    assert type_of_json is str  # assert str
+    try:
+        json.loads(most_similar_hybrid_by_tfidf)
+        assert True
+    except ValueError:
+        pytest.fail("Encountered an unexpected exception on trying to load JSON.")
+
