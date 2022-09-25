f17b972589c94eb9dcd426d01300b7983ac5f24c
patmull <patmull@seznam.cz>
2022-09-25 21:49:29 +0200

    Added none support to text join

---
+++ src/constants/naming.py
@@ -0,0 +1,4 @@
+class Naming:
+    REDIS_DELIMITER = ':'
+    PREDICTED_BY_THUMBS_REDIS_KEY_NAME = 'thumbs-ratings'
+    PREDICTED_BY_STARS_REDIS_KEY_NAME = 'stars-ratings'
--- try_prefillers.py
+++ try_prefillers.py
@@ -3,17 +3,23 @@
 # predict_ratings_for_all_users_store_to_redis()
 import os
 
+import spacy_sentence_bert
+
 from src.prefillers.user_based_prefillers.prefilling_collaborative import run_prefilling_collaborative
+from src.prefillers.user_based_prefillers.prefilling_user_classifier import prefill_for_user
 from src.recommender_core.data_handling.data_manipulation import DatabaseMethods
+from src.recommender_core.data_handling.data_queries import RecommenderMethods
+from src.recommender_core.recommender_algorithms.user_based_algorithms.user_relevance_classifier.classifier import \
+    Classifier
 
 """
 classifier = Classifier()
 try:
     bert = spacy_sentence_bert.load_doc2vec_model('xx_stsb_xlm_r_multilingual')
-    classifier.predict_relevance_for_user(user_id=431, relevance_by='stars', only_with_bert_vectors=False,
+    classifier.predict_relevance_for_user(user_id=431, relevance_by='stars', only_with_prefilled_bert_vectors=False,
                                           bert_model=bert, use_only_sample_of=20)
 
-    classifier.predict_relevance_for_user(user_id=431, relevance_by='stars', only_with_bert_vectors=False,
+    classifier.predict_relevance_for_user(user_id=431, relevance_by='stars', only_with_prefilled_bert_vectors=False,
                                           bert_model=bert, use_only_sample_of=20, force_retraining=True)
     r = get_redis_connection()
     print(r.smembers('posts_by_pred_ratings_user_431'))
@@ -48,7 +54,23 @@ assert 1 == mockconnect.call_count
 assert mockconnect.call_args_list[0] == call(user=DB_USER, password=DB_PASSWORD,
                                              host=DB_HOST, dbname=DB_NAME)
 """
+import logging
+
+
+log_format = '[%(asctime)s] [%(levelname)s] - %(message)s'
+logging.basicConfig(level=logging.DEBUG, format=log_format)
+logging.debug("Testing logging.")
 
 # TODO: Prefill SVD again (was partially rewritten by keywords)
 # run_prefilling_svd()
-run_prefilling_collaborative(test_run=True)
+# run_prefilling_collaborative(test_run=True)
+
+test_user_id = 431
+recommender_methods = RecommenderMethods()
+all_users_df = recommender_methods.get_users_dataframe()
+classifier = Classifier()
+print("Loading BERT multilingual model...")
+bert = spacy_sentence_bert.load_model('xx_stsb_xlm_r_multilingual')
+classifier.predict_relevance_for_user(user_id=test_user_id, relevance_by='thumbs', force_retraining=False,
+                                      bert_model=bert, use_only_sample_of=None, only_with_prefilled_bert_vectors=False,
+                                      experiment_mode=False)
