27b0019b40556c25f984e3e04ef9fda4215a737d
patmull <patmull@seznam.cz>
2022-10-25 14:50:53 +0200

    Implemented deriving of hybrid post freshness values and fixed tests

--- tests/test_unit/test_recommender_methods/test_hybrid_methods.py
+++ tests/test_unit/test_recommender_methods/test_hybrid_methods.py
@@ -81,27 +81,3 @@ def test_svm_classifier_bad_sample_number(tested_input):
     with pytest.raises(ValueError):
         svm = Classifier()
         assert svm.predict_relevance_for_user(use_only_sample_of=tested_input, user_id=431, relevance_by='stars')
-
-
-def test_boost_by_freshness():
-    coeff_1 = 0.5
-    coeff_2 = 0.8
-    coeff_3 = 0.6
-    coeff_4 = 0.856
-    now = datetime.now(timezone.utc).replace(tzinfo=None)
-    fresh = now + timedelta(minutes=-30)
-    older_than_hour = now + timedelta(hours=-3)
-    older_than_day = now + timedelta(days=-3)
-    older_than_5_days = now + timedelta(days=-6)
-    tested_data = {
-        'coefficient': [coeff_1, coeff_2, coeff_3, coeff_4],
-        'post_created_at': [fresh, older_than_hour, older_than_day, older_than_5_days]
-    }
-    tested_df = pd.DataFrame(tested_data)
-    tested_df = boost_by_article_freshness(tested_df)
-
-    hybrid_constants = HybridConstants()
-    assert tested_df.iloc[0]['coefficient'] == coeff_1 * hybrid_constants.coeff_and_hours_1[0]
-    assert tested_df.iloc[1]['coefficient'] == coeff_2 * hybrid_constants.coeff_and_hours_2[0]
-    assert tested_df.iloc[2]['coefficient'] == coeff_3 * hybrid_constants.coeff_and_hours_3[0]
-    assert tested_df.iloc[3]['coefficient'] == coeff_4 * hybrid_constants.coeff_and_hours_4[0]
