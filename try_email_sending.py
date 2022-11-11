363bd43bf3ba494628b0ab47abca2db5caca6e27
patmull <patmull@seznam.cz>
2022-11-11 20:42:30 +0100

    Added full exception to email

--- try_email_sending.py
+++ try_email_sending.py
@@ -1,3 +1,13 @@
+import traceback
+
 from mail_sender import send_error_email
 
-send_error_email("Testing message.")
+
+def email_sending():
+    raise ValueError("Testing exception")
+
+
+try:
+    email_sending()
+except Exception as e:
+    send_error_email(traceback.format_exc())
