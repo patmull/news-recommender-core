8a6dbfc01e354aeed71f634a13c6dfe556067da1
patmull <patmull@seznam.cz>
2022-11-19 12:21:49 +0100

    Added tested logrotate

--- run_rabbitmq_nonstop_thumbs.sh
+++ run_rabbitmq_nonstop_thumbs.sh
@@ -3,10 +3,6 @@
 while true; do
   # Local (laptop DELL):
   # nohup python3 ~/Documents/Codes/moje-clanky-core/news-recommender-core/rabbitmq_multithreaded_thumbs.py >> thumbs.out
-  # Previous version:
-  # nohup python3 /home/muller/Dokumenty/Codes/moje-clanky-api/rabbitmq_multithreaded_thumbs.py >> thumbs.out
-  # Experimental:
-  exec >  >(awk '{ print $0; fflush();}')
-  exec 2>  >(awk '{ print $0; fflush();}')
-  nohup python3 /home/muller/Dokumenty/Codes/moje-clanky-api/rabbitmq_multithreaded_thumbs.py >> thumbs.out &
+  # OSU Server:
+  nohup python3 /home/muller/Dokumenty/Codes/moje-clanky-api/rabbitmq_multithreaded_thumbs.py >> thumbs.out
 done &
