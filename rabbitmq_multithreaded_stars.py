f9cead983170af396a088eaad7dd239ddb3a7d31
patmull <patmull@seznam.cz>
2022-11-20 18:53:31 +0100

    Removed hybrid prefilling from keywords and scrapper

--- rabbitmq_multithreaded_stars.py
+++ rabbitmq_multithreaded_stars.py
@@ -56,7 +56,7 @@ def on_message(channel, method_frame, header_frame, body, args):
     logging.info("[x] Received %r" % body.decode())
     logging.info("Properties:")
     logging.info(header_frame)
-    channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+    # NOTICE: Basic ack should not be here. It is already acknowledged in do_work_ function
     if body.decode():
         if not is_init_or_test(body.decode()):
             logging.debug(ChannelConstants.USER_PRINT_CALLING_PREFILLERS)
