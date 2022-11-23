78e87acfd6ff4ae3f4f618753489d0f80d0ea8ba
patmull <patmull@seznam.cz>
2022-11-23 13:03:53 +0100

    Added script for reload of services and cleaning disk space

---
+++ deploy/new_deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+systemctl --user daemon-reload
+systemctl --user restart python_categories
+systemctl --user restart python_keywords
+systemctl --user restart python_scraper
+systemctl --user restart python_stars
+systemctl --user restart python_thumbs
