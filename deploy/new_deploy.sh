c8955c341be7c2947890f0d8add6e877b9b90f84
patmull <54801581+patmull@users.noreply.github.com>
2022-11-25 11:40:56 +0100

    Add new service for hybird prefillong to reloading script

--- deploy/new_deploy.sh
+++ deploy/new_deploy.sh
@@ -6,3 +6,4 @@ systemctl --user restart python_keywords
 systemctl --user restart python_scraper
 systemctl --user restart python_stars
 systemctl --user restart python_thumbs
+systemctl --user restart python_hybrid_prefilling
