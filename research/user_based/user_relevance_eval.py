309d2b28e67d8e47ee6151ee77aab780fb599456
patmull <patmull@seznam.cz>
2022-12-02 09:39:01 +0100

    Added statistics saving to Redis

--- research/user_based/user_relevance_eval.py
+++ research/user_based/user_relevance_eval.py
@@ -6,12 +6,14 @@ import pandas as pd
 from sklearn.metrics import average_precision_score, precision_score, balanced_accuracy_score, confusion_matrix, \
     dcg_score, f1_score, jaccard_score, ndcg_score, precision_recall_curve, top_k_accuracy_score
 
+from src.constants.naming import Naming
+from src.recommender_core.data_handling.data_manipulation import get_redis_connection
 from src.recommender_core.data_handling.model_methods.user_methods import UserMethods
 from src.recommender_core.recommender_algorithms.user_based_algorithms.user_relevance_classifier.user_evaluation_results import \
     get_user_evaluation_results_dataframe
 
 
-def user_relevance_asessment():
+def user_relevance_asessment(save_to_redis=True):
     user_methods = UserMethods()
     user_id_list = user_methods.get_all_users()['id'].tolist()
     precision_score_weighted_list = []
@@ -87,3 +89,19 @@ def user_relevance_asessment():
 
     output_file = Path("research/user_based/user_eval_results.csv")
     df.to_csv(output_file.as_posix(), header=True)
+
+    if save_to_redis:
+        REDIS_TOP_FOLDER = 'statistics'
+        testing_redis_key = REDIS_TOP_FOLDER + Naming.REDIS_DELIMITER + 'testing-redis-key'
+        r = get_redis_connection()
+        if "PYTEST_CURRENT_TEST" in os.environ:
+            r.set(testing_redis_key, 'testing-redis-value')
+        else:
+            list_of_columns = df.columns.tolist()
+            list_of_columns.remove('sections_list')
+            for section in df['sections_list'].tolist():
+                for column_name in list_of_columns[1:]:
+                    redis_key = REDIS_TOP_FOLDER + Naming.REDIS_DELIMITER + section + Naming.REDIS_DELIMITER \
+                                + column_name
+                    stat_value = df.loc[df['sections_list'] == section, column_name].iloc[0]
+                    r.set(redis_key, float(stat_value))
