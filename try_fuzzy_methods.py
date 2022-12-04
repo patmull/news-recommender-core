8bcc5cda8b4ab14141a9e01a710e8d175c2e0ef0
patmull <patmull@seznam.cz>
2022-12-04 12:27:12 +0100

    Continuing with integration of fuzzy

--- try_fuzzy_methods.py
+++ try_fuzzy_methods.py
@@ -1,9 +1,9 @@
 from research.fuzzy.fuzzy_experiments_proof_of_concepts import inference_simple_mamdani_boosting_coeff, \
-    inference_simple_mamdani_ensembling_ratio, fuzzy_weights_coeff
+    fuzzy_weights_coeff
 
 if __name__ == '__main__':
     inference_simple_mamdani_boosting_coeff()
-    inference_simple_mamdani_ensembling_ratio()
+
     fuzzy_weights_coeff()
     # example_fuzzy_sets()
     # example_output_space()
