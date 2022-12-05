57d289b4b09a49bb65c7e71d9167680045e9e113
patmull <patmull@seznam.cz>
2022-12-05 21:52:12 +0100

    Added new fuzzy

--- try_fuzzy_methods.py
+++ try_fuzzy_methods.py
@@ -1,8 +1,11 @@
 from src.recommender_core.recommender_algorithms.fuzzy.fuzzy_mamdani_inference import \
-    inference_mamdani_boosting_coeff
+    inference_mamdani_boosting_coeff, inference_simple_mamdani_cb_mixing
+
 if __name__ == '__main__':
     similarity = 0.85
     freshness = 3
     print(inference_mamdani_boosting_coeff(similarity, freshness))
+    days = 2
+    print(inference_simple_mamdani_cb_mixing(similarity, days, 'tfidf'))
     # example_fuzzy_sets()
     # example_output_space()
