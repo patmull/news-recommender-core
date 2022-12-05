f8d358dee8b57f5feb43081a4bf34aa79da3dcbf
patmull <patmull@seznam.cz>
2022-12-05 12:25:43 +0100

    Added LFLC

---
+++ research/fuzzy/design lflc/temp.rb
@@ -0,0 +1,140 @@
+!COMMENT!
+Problem solved: Amplifying the impact of fresh news in the article recommendation system
+
+
+
+Similarity: U = [0;1]; similarity of pairs of articles based on content-based algorithms; Cosine-Similarity or Jensen-Shannon; (0 = pairs of articles are not similar at all; 1 = pairs of articles identical)
+
+
+Freshness: V = [0; 336]; freshness of articles; in hours; (0 = article is newer than 1 hour; 336 = approx. half of a month old, its age is no longer meaningful to track)
+
+
+-----------
+
+Boosting coefficient: W = [0; 1]; Provides the boost based on the similarity and freshness of the article
+
+Fuzzy decomposition of universes for individual variables. The individual fuzzy sets that will form this fuzzy decomposition are then appropriately named. In this example, the decomposition of the universe of variables will always consist of three fuzzy sets:
+
+Similarity: Very high, High, Medium, Low, Very low 
+Freshness: Very fresh, fresh, current, slightly old, old
+Boosting coefficient: Very high, high, medium, low, very low
+!END_COMMENT!
+
+TypeOfDescription=linguistic
+InfMethod=Fuzzy_Approximation-functional
+DefuzzMethod=SimpleCenterOfGravity
+UseFuzzyFilter=false
+
+NumberOfAntecedentVariables=2
+NumberOfSuccedentVariables=1
+NumberOfRules=6
+
+AntVariable1
+ name=similarity
+ settings=new
+ context=<0,0.5,1>
+ discretization=301
+ UserTerm
+  name=very_low
+  type=trapezoid
+  parameters= 0 0 0.2 0.4
+ End_UserTerm
+ UserTerm
+  name=low
+  type=trapezoid
+  parameters= 0.2 0.3 0.4 0.45
+ End_UserTerm
+ UserTerm
+  name=med
+  type=trapezoid
+  parameters= 0.4 0.45 0.55 0.6
+ End_UserTerm
+ UserTerm
+  name=high
+  type=trapezoid
+  parameters= 0.7 0.75 0.8 0.898
+ End_UserTerm
+ UserTerm
+  name=very_high
+  type=trapezoid
+  parameters= 0.8 0.9 1 1
+ End_UserTerm
+ UserTerm
+  name=medium_high
+  type=trapezoid
+  parameters= 0.55 0.6 0.7 0.75
+ End_UserTerm
+End_AntVariable1
+
+AntVariable2
+ name=freshness
+ settings=new
+ context=<0,72,336>
+ discretization=301
+ UserTerm
+  name=old
+  type=trapezoid
+  parameters= 168 192 336 336
+ End_UserTerm
+ UserTerm
+  name=slightly_old
+  type=trapezoid
+  parameters= 120 144 168 193
+ End_UserTerm
+ UserTerm
+  name=current
+  type=trapezoid
+  parameters= 72 96 120 144
+ End_UserTerm
+ UserTerm
+  name=fresh
+  type=trapezoid
+  parameters= 24 48 72 96
+ End_UserTerm
+ UserTerm
+  name=very_fresh
+  type=trapezoid
+  parameters= 0 0 24 48
+ End_UserTerm
+End_AntVariable2
+
+SucVariable1
+ name=boosting
+ settings=new
+ context=<0,0.5,1>
+ discretization=301
+ UserTerm
+  name=very_high
+  type=trapezoid
+  parameters= 0.7 0.8 1 1
+ End_UserTerm
+ UserTerm
+  name=high
+  type=trapezoid
+  parameters= 0.55 0.6 0.7 0.8
+ End_UserTerm
+ UserTerm
+  name=med
+  type=trapezoid
+  parameters= 0.4 0.45 0.55 0.6
+ End_UserTerm
+ UserTerm
+  name=low
+  type=trapezoid
+  parameters= 0.2 0.3 0.4 0.45
+ End_UserTerm
+ UserTerm
+  name=very_low
+  type=trapezoid
+  parameters= 0 0 0.2 0.3
+ End_UserTerm
+End_SucVariable1
+
+RULES
+ "very_high" "very_fresh" | "very_high"
+ "high" "fresh" | "high"
+ "medium_high" "current" | "med"
+ "med" "current" | "med"
+ "low" "slightly_old" | "low"
+ "very_low" "old" | "very_low"
+END_RULES
