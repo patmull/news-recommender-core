f732bb9dedb4c5268369259939c05d2aaae82b4e
patmull <patmull@seznam.cz>
2023-01-01 16:30:32 +0100

    Choosing ratings based on category and group

--- research/svd/rating_estimates/normalization.py
+++ research/svd/rating_estimates/normalization.py
@@ -1,15 +1,20 @@
+import random
 import sys
 
+import gensim
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
+from src.recommender_core.data_handling.data_queries import RecommenderMethods
 
-def load_data():
+
+def load_rating_estimation():
     categories_views = pd.read_csv('research/svd/rating_estimates/news-readers-behaviour-mapped-to-idnes-categories.csv',
                                    delimiter=';')
     print(categories_views.to_string())
     original_df = categories_views
-    categories_views = categories_views.fillna(0)
+    # categories_views = categories_views.fillna()
     num_cols = categories_views.columns.values.tolist()
     num_cols.remove('kategorie')
     categories_views = categories_views[num_cols].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
@@ -18,13 +23,70 @@ def load_data():
     normalized_df['kategorie'] = original_df['kategorie']
     print(normalized_df.to_string())
     normalized_df = normalized_df.set_index('kategorie')
+    normalized_df = normalized_df.where(normalized_df > 1.0, 1.0)
     print(normalized_df.to_string())
+    filter_cols = [col for col in normalized_df if col.startswith('Reuters')]
+    for filter_col in filter_cols:
+        normalized_df[filter_col]['moda'] = np.nan
+    print(normalized_df.to_string())
+
+    return normalized_df
+
+
+def show_categories_plots(df):
+    num_cols = df.columns.values.tolist()
+    num_cols.remove('kategorie')
     for num_column in num_cols:
-        normalized_df[num_column].plot(kind="bar")
+        df[num_column].plot(kind="bar")
         plt.title(num_column)
         plt.tight_layout()
         plt.xticks(rotation=45)
         plt.show()
+
+# TODO:
+#   1. Random post
+#   2. Check if category is in rating_estimation_df columns
+#   3.
+
+
+def choose_value_for_random_post():
+    rating_estimation_df = load_rating_estimation()
+    recommender_methods = RecommenderMethods()
+    posts_categories_df = recommender_methods.get_posts_categories_dataframe()
+
+    while True:
+        random_post = posts_categories_df.sample()
+        # preprocessing categories names to macth the dataset
+        random_post_slug = random_post['slug'].iloc[0]
+        random_post_category = random_post['category_title'].iloc[0]
+        random_post_category = gensim.utils.simple_preprocess(random_post_category)
+
+        print("random_post_category")
+        print(random_post_category)
+
+        print("rating_estimation_df.index")
+        print(rating_estimation_df.index)
+
+        if random_post_category[0] in rating_estimation_df.index.tolist():
+            break
+
+    demographic_groups = rating_estimation_df.columns.to_list()
+    random_demographic_group = random.choice(demographic_groups)
+
+    chosen_rating_for_post = rating_estimation_df[random_demographic_group][random_post_category]
+
+    print("Random post slug:")
+    print(random_post_slug)
+
+    print("Random post category:")
+    print(random_post_category)
+
+    print("random_demographic_group:")
+    print(random_demographic_group)
+
+    print("chosen_rating_for_post")
+    print(chosen_rating_for_post)
+
 
 class Normalizer:
     pass
