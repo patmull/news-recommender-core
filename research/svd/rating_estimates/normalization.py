405dd839473b29764b8d86ed04e2c397b3fc015e
patmull <patmull@seznam.cz>
2023-01-02 12:08:57 +0100

    Normalized random noise

--- research/svd/rating_estimates/normalization.py
+++ research/svd/rating_estimates/normalization.py
@@ -16,19 +16,24 @@ def load_rating_estimation():
     print(categories_views.to_string())
     original_df = categories_views
     # categories_views = categories_views.fillna()
+    print("Normalizing...")
+    print("Removing non-numerical categories column...")
     num_cols = categories_views.columns.values.tolist()
     num_cols.remove('kategorie')
     categories_views = categories_views[num_cols].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
     categories_views = categories_views * 5.0
     normalized_df = categories_views.round(2)
+    print("Adding back categories...")
     normalized_df['kategorie'] = original_df['kategorie']
     print(normalized_df.to_string())
     normalized_df = normalized_df.set_index('kategorie')
     normalized_df = normalized_df.where(normalized_df > 1.0, 1.0)
+    print("Adding 1.0 to dataset where value < 1.0")
     print(normalized_df.to_string())
     filter_cols = [col for col in normalized_df if col.startswith('Reuters')]
     for filter_col in filter_cols:
         normalized_df[filter_col]['moda'] = np.nan
+    print("Adding back NaN...")
     print(normalized_df.to_string())
 
     return normalized_df
@@ -43,12 +48,6 @@ def show_categories_plots(df):
         plt.tight_layout()
         plt.xticks(rotation=45)
         plt.show()
-
-
-# TODO:
-#   1. Random post
-#   2. Check if category is in rating_estimation_df columns
-#   3.
 
 
 def choose_value_for_random_post():
@@ -76,6 +75,8 @@ def choose_value_for_random_post():
     print(rating_estimation_df)
 
     rating_estimation_df = generate_random_noise_for_df(rating_estimation_df)
+    rating_estimation_df = normalize(rating_estimation_df)
+    rating_estimation_df = rating_estimation_df.round(2)
 
     print("rating_estimation_df with random noise:")
     print(rating_estimation_df)
@@ -109,19 +110,33 @@ def choose_value_for_random_post():
 
     print("Random post ID:")
     print(random_post_id)
+    try:
+        rounded_rating_value = round(chosen_rating_for_post)
+        if rounded_rating_value > 5:
+            rounded_rating_value = 5
+        if rounded_rating_value < 1:
+            rounded_rating_value = 1
+        print("Rounded rating:")
+        print(rounded_rating_value)
 
-    rounded_rating_value = round(chosen_rating_for_post)
-    if rounded_rating_value > 5:
-        rounded_rating_value = 5
-    print("Rounded rating:")
-    print(rounded_rating_value)
-
-    # recommender_methods.database.insert_rating(random_post_id, rounded_rating_value)
+        recommender_methods.database.insert_rating(random_post_id, rounded_rating_value)
+    except ValueError as e:
+        print(e)
+        print("Value is NaN. Skipping")
+def normalize(df):
+    normalized_df = df.apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+    normalized_df = normalized_df * 5
+    print("Adding 1.0 to dataset where value < 1.0")
+    normalized_df = normalized_df.where(normalized_df > 1.0, 1.0)
+    filter_cols = [col for col in normalized_df if col.startswith('Reuters')]
+    for filter_col in filter_cols:
+        normalized_df[filter_col]['moda'] = np.nan
+    return normalized_df
 
 
 def generate_random_noise_for_df(clean_df):
 
-    mu, sigma = 0, 0.1
+    mu, sigma = clean_df.mean(), clean_df.std()
     # creating a noise with the same dimension as the dataset (2,2)
     noise = np.random.normal(mu, sigma, clean_df.shape)
 
